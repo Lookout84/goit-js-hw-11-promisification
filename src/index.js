80228b47573444fa924764ca92c011c422297966
Oleksandr Ivashchuk <thelookout84@gmail.com>
2021-01-13 17:36:19 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import css from "./css/styles.css";
+// import css from "./css/styles.css";
 
-const delay = (ms) => {
-  // Твой код
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(ms);
-    }, ms);
-  });
+// const delay = (ms) => {
+//   // Твой код
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = (time) => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  callback(updatedUsers);
 };
 
-const logger = (time) => console.log(`Resolved after ${time}ms`);
+const logger = (updatedUsers) => console.table(updatedUsers);
 
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+/*
+ * Сейчас работает так
+ */
+toggleUserState(users, "Mango", logger);
+toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
