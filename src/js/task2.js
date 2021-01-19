add8acc4134e9a244610d297ef1df20cf2be912b
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2021-01-19 21:29:30 +0200

    update

--- src/js/task2.js
+++ src/js/task2.js
@@ -1,28 +1,32 @@
 const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
-  
-  const toggleUserState = (allUsers, userName, callback) => {
-    const updatedUsers = allUsers.map(user =>
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = new Promise(
+    allUsers.call((user) =>
       user.name === userName ? { ...user, active: !user.active } : user,
-    );
-  
-    callback(updatedUsers);
-  };
-  
-  const logger = updatedUsers => console.table(updatedUsers);
-  
-  /*
-   * Сейчас работает так
-   */
-  toggleUserState(users, 'Mango', logger);
-  toggleUserState(users, 'Lux', logger);
-  
-  /*
-   * Должно работать так
-   */
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+    ),
+  );
+
+  return updatedUsers;
+};
+
+console.log(updatedUsers);
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
